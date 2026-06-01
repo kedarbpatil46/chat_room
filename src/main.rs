776973d163e977779133040e99dcf48354729340
Kedar-prog <kedar.p@rejolut.com>
2026-06-01 23:45:58 +0530

    Multi Client connection to chat channel, simple user name authentication, handling via tokio runtime lightweight tasks

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, os::unix::net::SocketAddr, sync::Arc};
+use std::{collections::HashMap, sync::Arc};
 
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
@@ -6,21 +6,21 @@ use tokio::{
     sync::{Mutex, broadcast},
 };
 
+
 struct User {
     user_name: String,
     message_count: u32,
+    user_name_entered: bool,
 }
 
 impl User {
-
-    pub fn new(name: String) -> User {
+    pub fn new(name: &String) -> User {
         User {
-            user_name: name,
-            message_count: 0
+            user_name: name.clone(),
+            message_count: 0,
+            user_name_entered: true,
         }
     }
-
-    pub fn find_user() {}
 }
 
 #[tokio::main]
@@ -34,7 +34,7 @@ async fn main() {
     println!("Chat server running");
 
     loop {
-        let (mut socket, address) = match listener.accept().await {
+        let (socket, address) = match listener.accept().await {
             Ok(soc) => soc,
             Err(err) => {
                 println!("Could not connect {}", err);
@@ -42,35 +42,19 @@ async fn main() {
             }
         };
 
-        let user_joined_msg = format!("{} joined the chat\n", address);
-        socket.write_all(user_joined_msg.as_bytes()).await.unwrap();
-
         let (reader, mut writer) = socket.into_split();
 
+        let mut reader = BufReader::new(reader);
         let tx = tx.clone();
-        let users = users.clone();
+        let val = users.clone();
+
 
         tokio::spawn(async move {
             let mut rx = tx.subscribe();
+            let mut username = String::new();
 
-            let read_task = tokio::spawn(async move {
-                let guard = users.lock().await;
-                let user = guard.get(&address.to_string());
-                match user {
-                    Some(user) => user,
-                    None => {
-                        let mut reader = BufReader::new(reader);
-
-                        let mut line = String::new();
-                        loop {
-                            line.clear();
-
-                            let bytes_read = reader.read_line(&mut line).await.unwrap();       
-                        }
-                    }
-                }
-
-                let mut reader = BufReader::new(reader);
+                let prompt_username = format!("Enter your username: \n");
+                writer.write_all(prompt_username.as_bytes()).await.unwrap();
 
                 let mut line = String::new();
                 loop {
@@ -79,31 +63,54 @@ async fn main() {
                     let bytes_read = reader.read_line(&mut line).await.unwrap();
 
                     if bytes_read == 0 {
-                        print!("Connection Closed");
+                        print!("Invalid username");
+                    } else {
+                        username = line.trim().to_string();
+                        let new_user = User::new(&username);
+                        let mut guard2 = val.lock().await;
+                        guard2.insert(address.to_string(), new_user);
+                        match tx.send(format!("[{}] joined the chat\n", line.trim().to_string())) {
+                            Ok(_) => {}
+                            Err(er) => println!("Error is {}", er),
+                        }
                         break;
                     }
-
-                    match tx.send(format!("[{}] {}", address, line)) {
-                        Ok(_) => {}
-                        Err(er) => println!("Error is {}", er),
-                    }
                 }
-            });
 
-            let write_task = tokio::spawn(async move {
-                loop {
-                    match rx.recv().await {
-                        Ok(msg) => writer.write_all(msg.as_bytes()).await.unwrap(),
-                        Err(err) => {
-                            println!("Could not send message {}", err);
+                let read_task = tokio::spawn(async move {
+                    let mut line = String::new();
+                    loop {
+                        line.clear();
+
+                        let bytes_read = reader.read_line(&mut line).await.unwrap();
+
+                        if bytes_read == 0 {
+                            print!("Connection Closed");
+                            let mut guard3 = val.lock().await;
+                            guard3.remove(&address.to_string());
                             break;
                         }
+                        
+                        match tx.send(format!("[{}] {}", username, line)) {
+                            Ok(_) => {}
+                            Err(er) => println!("Error is {}", er),
+                        }
                     }
-                }
-            });
+                });
 
-            read_task.await.unwrap();
-            write_task.await.unwrap();
+                let write_task = tokio::spawn(async move {
+                    loop {
+                        match rx.recv().await {
+                            Ok(msg) => writer.write_all(msg.as_bytes()).await.unwrap(),
+                            Err(err) => {
+                                println!("Could not send message {}", err);
+                                break;
+                            }
+                        }
+                    }
+                });
+                read_task.await.unwrap();
+                write_task.await.unwrap();
         });
     }
 }
